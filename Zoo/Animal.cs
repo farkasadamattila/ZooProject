7780bcb726d3643a296f793af855d2b2d59c840f
farkasadamattila <farkas.adam.attila@students.jedlik.eu>
2025-11-20 12:18:58 +0100

    A projekt létrehozása, alapdesign -és funckciók megcsinálása, JSON fájl létrehozása (ChatGPT-vel) és beolvasása

---
+++ Zoo/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Animal
+    {
+        
+            public int id { get; set; }
+            public string species { get; set; }
+            public string habitat { get; set; }
+            public string arrival_date { get; set; }
+    }
+}
